4068dd3022eff113e764fdd81f0b641c5a0dd225
Faith <faith.kamau@student.moringaschool.com>
2023-07-25 19:02:24 +0300

    Adding files to the migrate folder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,9 @@
 require './config/environment'
 
-class ApplicationController < Sinatra::Base
+class UserController < Sinatra::Base
 
   configure do
     set :default_content_type, 'application/json'
-  end
-
-  get "/" do
-    "The Feline world"
   end
 
 end
